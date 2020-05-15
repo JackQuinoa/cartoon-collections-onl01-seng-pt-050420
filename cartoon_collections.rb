2e753fc98d0763431d909f79c3352988d17ad2e6
Jack Quinoa <jquinoa208@gmail.com>
2020-05-15 18:28:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(strings)
-  strings.find { |element| if element == "#{cheese_types}" return element }
+  strings.find { |element| if element == "#{cheese_types}" then element }
   
  ## the array below is here to help
  cheese_types = ["cheddar", "gouda", "camembert"]
