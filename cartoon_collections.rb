e3ab27e57aff83b4f1e1a094051c5cd04639e49f
Jack Quinoa <jquinoa208@gmail.com>
2020-05-15 18:33:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(strings)
-  strings.find { |element| element == cheese_types.any? }
+  strings.find { |element| element == cheese_types.each}
  
  
  cheese_types = ["cheddar", "gouda", "camembert"]
