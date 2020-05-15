a13767690d7f31881ff7ada99abb657122da234a
Jack Quinoa <jquinoa208@gmail.com>
2020-05-15 18:30:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(strings)
-  strings.find { |element| element == #{cheese_types} }
-  
+  strings.find { |element| element == #{cheese_types} 
+  }
  ## the array below is here to help
  cheese_types = ["cheddar", "gouda", "camembert"]
 end
