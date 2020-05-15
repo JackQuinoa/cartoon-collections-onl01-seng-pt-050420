40733cd60be97a50a30a982f7de96660545965fe
Jack Quinoa <jquinoa208@gmail.com>
2020-05-15 18:09:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def long_planeteer_calls(calls)
   calls.any? { |call| call.length > 4 }
 end
 
-#def find_the_cheese(strings)
-  # strings.include?(#{cheese_types})
-  # the array below is here to help
- # cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(strings)
+  strings.include?(#{cheese_types})
+ ## the array below is here to help
+ cheese_types = ["cheddar", "gouda", "camembert"]
 #end
