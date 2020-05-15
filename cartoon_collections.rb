207427475ec9f7e24c2b51d6e833a674f61e34aa
Jack Quinoa <jquinoa208@gmail.com>
2020-05-15 17:54:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,9 @@ end
 
 def long_planeteer_calls(calls)
   calls.each { |call| if call.length > 4
-                        return true 
+                        true 
                       else
-                        return false 
+                        false 
                        end }
 end
 
